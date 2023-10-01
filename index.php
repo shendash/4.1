43a056eaf185d3c5a32f5210b58835a71ac7a93e
Дарья <137810221+shendash@users.noreply.github.com>
2023-10-01 15:18:51 +0300

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="http://dota2.com/game.php">
-    Пароль: <input type="text" name="username" value="gamer"><br>
-    Логин: <input type="password" name="user_password" value="123456qwerty"><br>
-    <input type="submit" value="Отправить">
-</form>
