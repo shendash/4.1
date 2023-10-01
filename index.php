30f08adfae8e19eabce0d9c20c61f63d1f98e4c6
Дарья <137810221+shendash@users.noreply.github.com>
2023-10-01 15:20:11 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<form method="POST" name="myForm" action="http://dota2.com/game.php">
+    Пароль: <input type="text" name="username" value="gamer"><br>
+    Логин: <input type="password" name="password" value="123456qwerty"><br>
+    <input type="submit" value="Отправить">
+</form>
